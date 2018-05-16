10f339273f6fc393a44e3a83171a585a453ea68a
joepuzzo <jgpuzzo2@gmail.com>
2018-05-16 15:58:41 -0400

    updated ObjectMap and added lots of unit tests

--- src/ObjectMap/index.js
+++ src/ObjectMap/index.js
@@ -28,22 +28,29 @@ function isObject (a) {
   return !Array.isArray(a) && typeof a === 'object' && a !== null
 }
 
-const buildMap = ( map, cursor, path ) => {
-  if( isArray(cursor) ) {
-    map.set(path, cursor)
-    cursor.forEach((value, i)=>{
-      buildMap( map, value, `${path}[${i}]`)
-    })
-  }
-  else if( isObject(cursor) ){
-    if( path !== '' ){
-      map.set(path, cursor);
-    }
+const buildMap = ( cursor ) => {
+  if( isObject(cursor) ){
+    // Cursor is an object so we need a new map
+    const map = new Map();
+    // Iterate over each field and set to result
     Object.keys(cursor).forEach((key)=>{
-      buildMap( map, cursor[key], path !== '' ? `${path}.${key}` : key )
+      map.set( key, buildMap( cursor[key] ) );
     })
-  } else {
-    map.set(path, cursor);
+    // Return our new map
+    return map;
+  } else if( isArray(cursor) ) {
+    // Cursor is an array so we need a new map
+    const map = new Map();
+    // Iterate over each element and set to result
+    cursor.forEach((value, i)=>{
+      map.set( i, buildMap( value ) );
+    });
+    // Return our new map
+    return map;
+  }
+  // Base case, cursor is not object or array
+  else {
+    return cursor;
   }
 };
 
@@ -51,29 +58,46 @@ class ObjectMap {
 
   constructor( object = {} ){
     this.object = object;
-    this.map = new Map();
-    buildMap( this.map, object, '' )
+    this.map = buildMap( object );
   }
 
-  get ( path ) {
-    return this.map.get(path);
+  get( path ){
+
+    const pathArray = makePathArray( path );
+
+    const get = ( cursor, key, nextKey, i ) => {
+      // Base case1: cursor is undefined so just return
+      if( cursor == null ){
+        return;
+      }
+      // Base case2: next key is undefined
+      if( nextKey == null ){
+        return cursor[ key ];
+      }
+      // Recur
+      return get( cursor[ key ], pathArray[i+1], pathArray[i+2], i+1);
+    }
+    // Start recuring
+    return get( this.object, pathArray[0], pathArray[1], 0 )
   }
 
   set( path, value ){
 
     const pathArray = makePathArray( path );
 
-    const set = ( cursor, key, nextKey, i, mapKey ) => {
+    const set = ( cursor, map, key, nextKey, i ) => {
 
       // Base case: next key is undefined
       if( nextKey == null ){
-        if( value == null ){
-          delete cursor[key];
-          this.map.delete( mapKey );
-          console.log("HERE!!", this.map.size);
-        } else {
-          cursor[ key ] = value;
-          this.map.set( mapKey, value );
+        // Only modify if there is a cursor
+        if( cursor ){
+          if( value == null ){
+            delete cursor[key];
+            map.delete(key);
+          } else {
+            cursor[key] = value;
+            map.set(key, value)
+          }
         }
         return;
       }
@@ -82,25 +106,30 @@ class ObjectMap {
       // need to initialize it
       if (typeof nextKey === 'number' && !isArray(cursor[key]) && value != null) {
         cursor[key] = [];
-        this.map.set( mapKey, cursor[key] );
+        map.set(key, new Map());
       }
       // If the next key is not a number and the cursor is not an object yet
       // we need to initialize it
       if (typeof nextKey !== 'number' && !isObject(cursor[key]) && value != null) {
         cursor[key] = {};
-        this.map.set( mapKey, cursor[key] );
+        map.set(key, new Map());
       }
 
+      // If the next cursor is an array or object then we pass down a new map
+      const nextMap = isObject(cursor[key]) || isArray(cursor[key]) ? map.get(key) : map;
+
       // Recur
-      set(
-        cursor[ key ],
-        pathArray[i+1],
-        pathArray[i+2],
-        i+1,
-        typeof nextKey === 'number' ? `${mapKey}[${nextKey}]` : `${mapKey}.${nextKey}`);
+      set( cursor[ key ], nextMap, pathArray[i+1], pathArray[i+2], i+1);
+
+      // Check map
+      if( nextMap.size === 0 ){
+        delete cursor[key];
+        map.delete(key);
+      }
     }
 
-    set( this.object, pathArray[0], pathArray[1], 0, pathArray[0] )
+    // Start recuring
+    set( this.object, this.map, pathArray[0], pathArray[1], 0 );
   }
 
 }
--- tests/ObjectMap.spec.js
+++ tests/ObjectMap.spec.js
@@ -3,6 +3,29 @@ import { expect } from 'chai';
 
 describe('ObjectMap', () => {
 
+  describe('constructor', () => {
+
+    it('create a map and an object', () => {
+      const expectedObj  = {foo:'bar'};
+      const expectedMap  = new Map([['foo','bar']]);
+      const objectMap = new ObjectMap({foo:'bar'});
+      const actualObj = objectMap.object;
+      const actualMap = objectMap.map;
+      expect( actualObj ).to.deep.equal(expectedObj);
+      expect( [...actualMap] ).to.deep.equal([...expectedMap]);
+    });
+
+    it('create a nested map and an object', () => {
+      const expectedObj = {foo:{bar:{baz:3}}};
+      const objectMap = new ObjectMap({foo:{bar:{baz:3}}});
+      const actualObj = objectMap.object;
+      const actualMap = objectMap.map;
+      expect( actualObj ).to.deep.equal(expectedObj);
+      expect( actualMap.get('foo').get('bar').get('baz') ).to.deep.equal(3);
+    });
+
+  });
+
   describe('get', () => {
 
     it('should get value from very nested obejct', () => {
@@ -48,6 +71,164 @@ describe('ObjectMap', () => {
   });
 
   describe('set', () => {
+
+    it('should set a value', () => {
+      const expected = 3;
+      const objectMap = new ObjectMap();
+      objectMap.set('foo', 3);
+      const actual = objectMap.get('foo');
+      expect( actual ).to.equal(expected);
+    });
+
+    describe('object', () => {
+
+      it('should set a nested value and initialize objects along the way', () => {
+        const expected = {foo:{bar:{baz:3}}};
+        const objectMap = new ObjectMap();
+        objectMap.set('foo.bar.baz', 3);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should set a nested value and initialize arrays along the way', () => {
+        const expected = {foo:[,[,3]]};
+        const objectMap = new ObjectMap();
+        objectMap.set('foo[1][1]',3);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should set a nested value and initialize arrays and objects along the way', () => {
+        const expected = {foo:[,{bar:[,3]}]}
+        const objectMap = new ObjectMap();
+        objectMap.set('foo[1].bar[1]',3);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should remove objects when they are empty after setting null', () => {
+        const expected = {}
+        const objectMap = new ObjectMap({foo:{bar:{baz:3}}});
+        objectMap.set('foo.bar.baz', null);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should NOT remove objects when they are NOT empty after setting null', () => {
+        const expected = {foo:{bar:{boo:4}}}
+        const objectMap = new ObjectMap({foo:{bar:{baz:3,boo:4}}});
+        objectMap.set('foo.bar.baz', null);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should remove array when they are empty after setting null', () => {
+        const expected = {};
+        const objectMap = new ObjectMap({foo:[,[,3]]});
+        objectMap.set('foo[1][1]',null);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should NOT remove array when they are NOT empty after setting null', () => {
+        const expected = {foo:[,[,,4]]};
+        const objectMap = new ObjectMap({foo:[,[,3,4]]});
+        objectMap.set('foo[1][1]',null);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should remove arrays and objects when they are empty after setting null', () => {
+        const expected = {};
+        const objectMap = new ObjectMap({foo:[,{bar:[,3]}]});
+        objectMap.set('foo[1].bar[1]',null);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+      it('should NOT remove arrays and objects when they are NOT empty after setting null', () => {
+        const expected = {foo:[,{bar:[,,4]}]};
+        const objectMap = new ObjectMap({foo:[,{bar:[,3,4]}]});
+        objectMap.set('foo[1].bar[1]',null);
+        const actual = objectMap.object;
+        expect( actual ).to.deep.equal(expected);
+      });
+
+    });
+
+    describe('map', () => {
+
+      it('should set a nested value and initialize maps along the way', () => {
+        const objectMap = new ObjectMap();
+        objectMap.set('foo.bar.baz', 3);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo').get('bar') instanceof Map).to.equal(true);
+      });
+
+      it('should set a nested value and initialize maps for arrays along the way', () => {
+        const objectMap = new ObjectMap();
+        objectMap.set('foo[1][1]',3);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo').get(1) instanceof Map).to.equal(true);
+      });
+
+      it('should set a nested value and initialize maps for arrays and maps for objects along the way', () => {
+        const objectMap = new ObjectMap();
+        objectMap.set('foo[1].bar[1]',3);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo').get(1) instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo').get(1).get('bar') instanceof Map).to.equal(true);
+      });
+
+      it('should remove maps when they are empty after setting null', () => {
+        const objectMap = new ObjectMap({foo:{bar:{baz:3}}});
+        objectMap.set('foo.bar.baz', null);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(false);
+      });
+
+      it('should NOT remove maps when they are NOT empty after setting null', () => {
+        const objectMap = new ObjectMap({foo:{bar:{baz:3,boo:4}}});
+        objectMap.set('foo.bar.baz', null);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo').get('bar') instanceof Map).to.equal(true);
+      });
+
+      it('should remove map when arrays are empty after setting null', () => {
+        const objectMap = new ObjectMap({foo:[,[,3]]});
+        objectMap.set('foo[1][1]',null);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(false);
+      });
+
+      it('should NOT remove map when arrays are are NOT empty after setting null', () => {
+        const objectMap = new ObjectMap({foo:[,[,3,4]]});
+        objectMap.set('foo[1][1]',null);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo').get(1) instanceof Map).to.equal(true);
+      });
+
+      it('should remove arrays and objects when they are empty after setting null', () => {
+        const objectMap = new ObjectMap({foo:[,{bar:[,3]}]});
+        objectMap.set('foo[1].bar[1]',null);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(false);
+      });
+
+      it('should NOT remove arrays and objects when they are NOT empty after setting null', () => {
+        const objectMap = new ObjectMap({foo:[,{bar:[,3,4]}]});
+        objectMap.set('foo[1].bar[1]',null);
+        expect(objectMap.map instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo') instanceof Map).to.equal(true);
+        expect(objectMap.map.get('foo').get(1).get('bar') instanceof Map).to.equal(true);
+      });
+
+    });
 
     it('should set value in very nested obejct', () => {
       const expected = 3;

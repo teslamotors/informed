ea03b936a97ab8b701242aeb7b0348ef4f10cff3
Michael Dombrowski <mikepi3.14@gmail.com>
2019-07-06 22:13:25 -0700

    Automatically grow/shrink arrayfield from updates

--- src/hooks/useArrayField.js
+++ src/hooks/useArrayField.js
@@ -41,55 +41,28 @@ const useArrayField = ({ field, initialValue, validate, ...props }) => {
 
   // NOTE: important that we use "field" and NOT full field as getter is scoped!
   const { fieldApi } = useField({ field, validate: validateWithLength, shadow: true, ...props });
+  
+  const [deferredUpdates, setDeferredUpdates] = React.useState({});
 
-  // Register for events
-  useLayoutEffect(()=>{
-
-    // Define event handler
-    const onChangeHandler = ( fieldName  ) => {
-
-      // Dont do anythign if we updated
-      if( fieldName === fullField ){
-        return;
-      }
-      
-      logger(`${fullField} changed`);
-
-      // determine if one of our array children triggered this change 
-      if( RegExp(`${fullField}\\[[0-9]+\\]`).test(fieldName) ) {
-        // If it was than update the shadow field!!! 
-        // NOTE: important that we use "field" and NOT full field as getter is scoped!
-        const arrayFieldValue = formApi.getValue(field);
-        logger(`setting array field ${fullField} to ${arrayFieldValue}`);
-        fieldApi.setValue(arrayFieldValue);
-      }
-
-    };
-
-    // Register for events
-    formApi.emitter.on('value', onChangeHandler);
-
-    // Unregister events
-    return () => {
-      formApi.emitter.removeListener('value', onChangeHandler);
-    };
-  }, [field]);
-
-
-
-  const remove = i => {
-    const newKeys = keys.slice(0, i).concat(keys.slice(i + 1, keys.length));
+  const remove = (i) => {
+    const newKeys = getKeys()
+      .slice(0, i)
+      .concat(getKeys().slice(i + 1, getKeys().length));
     setKeys(newKeys);
     const newInitialValues = initialValues.slice(0, i).concat(initialValues.slice(i + 1, initialValues.length));
     setInitialValues(newInitialValues);
+    deferredUpdates[`${field}[${i}]`] = null;
+    setDeferredUpdates(deferredUpdates);
   };
 
   const add = () => {
+    const keys = getKeys();
     keys.push(uuidv4());
     setKeys([...keys]);
   };
 
-  const addWithInitialValue = ( initialValue ) => {
+  const addWithInitialValue = (initialValue) => {
+    const keys = getKeys();
     keys.push(uuidv4());
     setKeys([...keys]);
     const newInitialValues = [...initialValues];
@@ -97,12 +70,78 @@ const useArrayField = ({ field, initialValue, validate, ...props }) => {
     setInitialValues(newInitialValues);
   };
 
-  const fields = keys.map((key, i) => ({
+  // Register for events
+  useIsomorphicLayoutEffect(() => {
+    // Define event handler
+    const onChangeHandler = (fieldName, v) => {
+      const values = formApi.getValue(field);
+      if (fieldName === fullField) {
+        if (values !== undefined && Array.isArray(values)) {
+          const makeDeferredUpdates = (v, prefix) => {
+            // If the field exists, then it would have already been set. So ignore it here.
+            if (formApi.fieldExists(prefix)) {
+              return;
+            }
+
+            if (Array.isArray(v)) {
+              deferredUpdates[prefix] = v;
+            } else if (typeof v === "object") {
+              Object.keys(v).forEach((key) => {
+                makeDeferredUpdates(v[key], prefix + "." + key);
+              });
+            } else {
+              deferredUpdates[prefix] = v;
+            }
+          };
+
+          for (let i = getKeys().length; i < values.length; i++) {
+            add();
+            makeDeferredUpdates(values[i], `${fullField}[${i}]`);
+            setDeferredUpdates(deferredUpdates);
+          }
+
+          // Only delete when no changes have been deferred
+          if (Object.keys(deferredUpdates).length === 0) {
+            for (let i = values.length + 1; i <= getKeys().length; i++) {
+              remove(i - 1);
+            }
+          }
+        }
+      }
+
+      // determine if one of our array children triggered this change
+      if (RegExp(`${fullField}\\[[0-9]+\\]`).test(fieldName)) {
+        // If it was then update the shadow field!!!
+        // NOTE: important that we use "field" and NOT full field as getter is scoped!
+        const arrayFieldValue = formApi.getValue(field);
+        fieldApi.setValue(arrayFieldValue);
+      }
+    };
+
+    // Register for events
+    formApi.emitter.on("value", onChangeHandler);
+
+    // Unregister events
+    return () => {
+      formApi.emitter.removeListener("value", onChangeHandler);
+    };
+  }, [field]);
+
+  const fields = getKeys().map((key, i) => ({
     key,
     field: `${field}[${i}]`,
     remove: () => remove(i),
     initialValue: initialValues && initialValues[i]
   }));
+
+  Object.keys(deferredUpdates).forEach((field) => {
+    if (formApi.fieldExists(field)) {
+      const v = deferredUpdates[field];
+      delete deferredUpdates[field];
+      setDeferredUpdates(deferredUpdates);
+      formApi.setValue(field, v || "");
+    }
+  });
 
   return { fields, add, addWithInitialValue };
 };

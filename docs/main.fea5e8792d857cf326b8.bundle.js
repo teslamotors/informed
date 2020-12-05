5994d41a6e4b372b5e23123241d3361c0cbe22ac
joepuzzo <jgpuzzo2@gmail.com>
2020-12-05 08:12:05 -0800

    updated docs

---
+++ docs/main.fea5e8792d857cf326b8.bundle.js
@@ -0,0 +1,27 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    543: function(o, n, e) {
+      e(554), e(899), e(948), e(1663), (o.exports = e(553));
+    },
+    685: function(o, n) {},
+    899: function(o, n, e) {
+      'use strict';
+      e.r(n);
+      var s = e(134);
+      s.addons.setConfig({
+        name: 'Informed',
+        url: 'https://joepuzzo.github.io/informed',
+        isFullscreen: !1,
+        showNav: !0,
+        showPanel: !1,
+        panelPosition: 'bottom',
+        sidebarAnimations: !0,
+        enableShortcuts: !0,
+        isToolshown: !0,
+        selectedPanel: void 0
+      });
+    }
+  },
+  [[543, 180, 181]]
+]);

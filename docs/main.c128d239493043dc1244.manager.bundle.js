e740c7a858e187f6daff073612743e11bced09e1
joepuzzo <jgpuzzo2@gmail.com>
2022-03-22 09:34:16 -0700

    rebeccapurple

---
+++ docs/main.c128d239493043dc1244.manager.bundle.js
@@ -0,0 +1,30 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [1],
+  {
+    1051: function(module, exports) {},
+    1196: function(module, __webpack_exports__, __webpack_require__) {
+      'use strict';
+      __webpack_require__.r(__webpack_exports__),
+        __webpack_require__(80).c.setConfig({
+          name: 'Informed',
+          url: 'https://teslamotors.github.io/informed',
+          isFullscreen: !1,
+          showNav: !0,
+          showPanel: !1,
+          panelPosition: 'bottom',
+          sidebarAnimations: !0,
+          enableShortcuts: !0,
+          isToolshown: !0,
+          selectedPanel: void 0
+        });
+    },
+    928: function(module, exports, __webpack_require__) {
+      __webpack_require__(929),
+        __webpack_require__(1196),
+        __webpack_require__(713),
+        __webpack_require__(1561),
+        (module.exports = __webpack_require__(1644));
+    }
+  },
+  [[928, 2, 3]]
+]);

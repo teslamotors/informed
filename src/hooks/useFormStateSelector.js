cf3afe26553b93ceb8c9da8558cc4e58be183640
Robin Ambachtsheer <rambachtsheer@tesla.com>
2022-08-04 15:31:50 +0200

    Code improvements in useFormStateSelector

--- src/hooks/useFormStateSelector.js
+++ src/hooks/useFormStateSelector.js
@@ -1,16 +1,6 @@
-import {
-  useMemo,
-  useRef,
-  useCallback,
-  useEffect,
-  useLayoutEffect
-} from 'react';
+import { useMemo, useRef, useEffect, useLayoutEffect } from 'react';
 import { useFormController } from './useFormController';
-import {
-  createDeepProxy,
-  isDeepChanged,
-  unwrap as unwrapProxy
-} from '../proxy';
+import { createDeepProxy, isDeepChanged } from '../proxy';
 import { useForceUpdate } from './useForceUpdate';
 
 if (typeof window.structuredClone !== 'function') {
@@ -21,8 +11,6 @@ if (typeof window.structuredClone !== 'function') {
     return JSON.parse(JSON.stringify(obj));
   };
 }
-
-export const unwrap = unwrapProxy;
 
 export function useFormStateSelector(selector) {
   const formController = useFormController();
@@ -39,31 +27,31 @@ export function useFormStateSelector(selector) {
     lastAffected.current = affected;
   });
 
-  const callback = useCallback(() => {
-    if (
-      isDeepChanged(
-        prevState.current,
-        formController.state,
-        lastAffected.current
-      )
-    ) {
-      prevState.current = structuredClone(formController.state);
-
-      selectorStateRef.current = selector(
-        createDeepProxy(prevState.current, affected, proxyCache)
-      );
-
-      forceUpdate();
-    }
-  }, []);
-
   useEffect(() => {
+    const callback = () => {
+      if (
+        isDeepChanged(
+          prevState.current,
+          formController.state,
+          lastAffected.current
+        )
+      ) {
+        prevState.current = structuredClone(formController.state);
+
+        selectorStateRef.current = selector(
+          createDeepProxy(prevState.current, affected, proxyCache)
+        );
+
+        forceUpdate();
+      }
+    };
+
     formController.on('field', callback);
 
     return () => {
       formController.removeListener('field', callback);
     };
-  });
+  }, []);
 
   return selectorStateRef.current;
 }

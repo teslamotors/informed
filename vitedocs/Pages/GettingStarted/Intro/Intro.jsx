7b0d0b6a9b9071aafde74a756f50af82247ced48
joepuzzo <jgpuzzo2@gmail.com>
2023-03-16 23:11:08 -0700

    styled inputs

--- vitedocs/Pages/GettingStarted/Intro/Intro.jsx
+++ vitedocs/Pages/GettingStarted/Intro/Intro.jsx
@@ -10,9 +10,15 @@ export default function Intro() {
   return (
     <>
       <h1>Getting Started</h1>
-      Install with npm
-      <code className="code-block">npm install --save informed</code>
+      <h2>Install</h2>
+      <code className="code-block">npm install informed</code>
+      <hr />
+      <h2>Code:</h2>
+      By default informed gives you inputs that have already been "hooked" up.
+      All you need to do is style the inputs.
       <Code input1={code} input2={css} language2="css" minWidth2="200px" />
+      <hr />
+      <h2>Output:</h2>
       <Example />
     </>
   );

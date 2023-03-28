4184a125e8f21a24fee395a8c18518010b40b968
joepuzzo <jgpuzzo2@gmail.com>
2023-03-28 05:59:35 -0700

    rebuilt docs

--- vitedocs/Routes/Routes.jsx
+++ vitedocs/Routes/Routes.jsx
@@ -52,9 +52,7 @@ import { RickRoll } from '../Pages/Examples/ElonMusk/RickRoll';
 const RootRoute = () => {
   const searchParams = new URLSearchParams(window.location.search);
   const to = searchParams.get('to');
-  console.log('TO', to);
   if (to) {
-    console.log('WTF');
     return <Navigate to={to} />;
   } else {
     return <Navigate to="getting-started/intro" replace />;
